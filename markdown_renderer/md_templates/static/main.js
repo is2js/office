f2fd180071b7d01a212d64a133be969178584e03
is2js <tingstyle1@gmail.com>
2025-05-18 14:43:06 +0900

    feat: 전체화면을 아이콘으로 변경

--- markdown_renderer/md_templates/static/main.js
+++ markdown_renderer/md_templates/static/main.js
@@ -1,8 +1,3 @@
 window.addEventListener('load', function(e) {
     document.querySelector('.footer #copyright #year').innerHTML = new Date().getFullYear() + ' ';
 });
-
-
-function getCurrentPath() {
-    return window.location.pathname
-}
