14726cd56030bdaec0aa10a1296be95c00885512
is2js <tingstyle1@gmail.com>
2025-05-18 10:29:54 +0900

    feat: fullpage 원내광고를 markdown으로 이식하기

--- markdown_renderer/md_templates/static/main.js
+++ markdown_renderer/md_templates/static/main.js
@@ -1,3 +1,8 @@
 window.addEventListener('load', function(e) {
     document.querySelector('.footer #copyright #year').innerHTML = new Date().getFullYear() + ' ';
 });
+
+
+function getCurrentPath() {
+    return window.location.pathname
+}
